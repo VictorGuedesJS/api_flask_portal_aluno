f447bdf35a59ea2c64286ef9c49ef973dab3f56c
VictorGuedesJS <victorguedes.js@gmail.com>
2024-07-30 21:50:25 -0400

    add get cursos and post cursos

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from db import pessoas, cursos, alunos, professores
+import json
 
 
 app = Flask(__name__)
 
-@app.route('pessoas', method = ['GET'])
+@app.route('/pessoas', methods = ['GET'])
 def getPesoas():
     return make_response(
         jsonify(pessoas)
     )
+@app.route('/cursos', methods = ['GET'])
+def getCursos():
+    return make_response(
+        jsonify(cursos)
+    )
+@app.route('/cursos', methods = ['POST'])
+def postCursos():
+    data = json.loads(request.json)
+    tamanhoDict = len(cursos)
+    cursos[tamanhoDict + 1] = data
+    print(cursos[tamanhoDict + 1])
+    return "Sucessful"
+    
 
 app.run()
