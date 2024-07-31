3b7a0bdb06e0557210807d8a0300590064986f3b
VictorGuedesJS <victorguedes.js@gmail.com>
2024-07-30 21:29:38 -0400

    Add Get pessoas

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from db import pessoas, cursos, alunos, professores
 
 
 app = Flask(__name__)
+
+@app.route('pessoas', method = ['GET'])
+def getPesoas():
+    return make_response(
+        jsonify(pessoas)
+    )
 
 app.run()
