7fc15db64e7d05387c9b72395733c93f1b23dc23
VictorGuedesJS <victorguedes.js@gmail.com>
2024-08-01 18:50:41 -0400

    Adicionando VIew

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from pessoaController import pessoas_bp
-from cursosController import cursos_bp
+from Controllers.pessoaController import pessoas_bp
+from Controllers.cursosController import cursos_bp
+from Controllers.professoresController import professores_bp
 
 
 app = Flask(__name__)
 app.register_blueprint(pessoas_bp)
 app.register_blueprint(cursos_bp)
-
+app.register_blueprint(professores_bp)
 
 app.run()
